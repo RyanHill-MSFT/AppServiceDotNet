cae986ea22bd1dc43e03b5ff6213eec9794726e7
Ryan Hill <hill.ryan@microsoft.com>
2022-05-25 10:55:36 -0400

    common initial commit

---
+++ common/models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace common.models
+{
+    public class Todo
+    {
+        [JsonProperty]
+        public Guid Id { get; set; }
+        [JsonProperty]
+        public int? Order { get; set; }
+        [JsonProperty]
+        public string Title { get; set; }
+        [JsonProperty]
+        public string Description { get; set; }
+        [JsonProperty]
+        public DateTime? DueDate { get; set; }
+        [JsonProperty]
+        public DateTime Created { get; private set; } = DateTime.Now;
+        [JsonProperty]
+        public bool? Completed { get; set; }
+    }
+}
