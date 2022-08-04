3513509e0f41fc51e3c5118b2c1630d9ed30b73d
Ryan Hill <hill.ryan@microsoft.com>
2022-08-04 10:46:57 -0400

    webjob initial checkin

---
+++ webjob/Functions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace webjob
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, ILogger log)
+        {
+            log.LogInformation(message);
+        }
+    }
+}
---
+++ webjob/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = new HostBuilder();
+builder
+    .UseEnvironment(EnvironmentName.Development)
+    .ConfigureWebJobs(b =>
+    {
+        b.AddAzureStorageCoreServices();
+        b.AddAzureStorageQueues();
+    })
+    .ConfigureLogging((context, b) =>
+    {
+        b.AddConsole();
+
+        // If the key exists in settings, use it to enable Application Insights.
+        string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+        if (!string.IsNullOrEmpty(instrumentationKey))
+        {
+            b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+        }
+    });
+
+var host = builder.Build();
+using (host)
+{
+    await host.RunAsync();
+}
